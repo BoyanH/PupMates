608cfda7376b751604722aece885249e04fab13c
BoyanH <boqnh@abv.bg>
2014-12-09 14:03:12 +0200

    github not adding new folders, added manually

---
+++ public/app/services/socketioService.js
@@ -0,0 +1,51 @@
+app.factory('socket', ['identity', 'rootScope'], function(identity, $rootScope) {
+   
+    var socket = io('/'),
+        connected = false;
+
+    socket.on('who are you', function() {
+
+        socket.emit('check in', {
+            userId: identity.currentUser._id
+        });
+    });
+
+    socket.on('registered', function() {
+
+        connected = true;
+    })
+
+    return {
+        on: function(eventName, callback) {
+            
+            socket.on(eventName, function() {
+            
+                var args = arguments;
+            
+                $rootScope.$apply(function() {
+                    callback.apply(socket, args);
+                });
+
+            });
+        },
+
+        emit: function(eventName, data, callback) {
+
+            socket.emit(eventName, data, function() {
+                
+                var args = arguments;
+               
+                $rootScope.$apply(function() {
+
+                    if (callback) {
+
+                        callback.apply(socket, args);
+                    }
+
+                });
+            })
+        },
+
+        isConnected: connected
+    };
+});
