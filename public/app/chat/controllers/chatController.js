4bc7de3dbdb1dd6a6f0a9c03594aec474c5f2116
unknown <alexanderyordanov96@gmail.com>
2014-12-10 01:52:41 +0200

    CSS For Login Dialog and Search

--- public/app/chat/controllers/chatController.js
+++ public/app/chat/controllers/chatController.js
@@ -1,12 +1,8 @@
 'use strict';
-<<<<<<< HEAD
-app.controller('ChatController', function($scope, identity, $routeParams, socket){
-=======
 app.controller('ChatController', function($scope, identity, $routeParams){
     //set height of the left menu
     var height = $(document).height() - $(".nav").height();
     $(".menu").css("height", height.toString());
->>>>>>> 2666ec126b8531ecd7f58ee4d8eec6d5c2b85c7e
 
 	if ($routeParams.friendId) {
 		//load conversation
