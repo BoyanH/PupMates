2371b997c9be63573f3311133327c12ff471d62e
unknown <alexanderyordanov96@gmail.com>
2014-12-10 21:22:33 +0200

    Almost ready left Dialog design

--- public/app/main/controllers/menuController.js
+++ public/app/main/controllers/menuController.js
@@ -1,17 +1,33 @@
 app.controller("MenuController", function($scope){
-	$("#left-menu-dialog").height($(document).height()-$(".nav").height() - 3);
+	$("#left-menu-dialog").height($(document).height()-$(".nav").height());
+
     $scope.showDialog = function(){
     	var wd = $("#left-menu-dialog").width();
     	if(wd == 380){
     		$("#left-menu-dialog").width(0);
     		$("#cover").css("display", "none");
+            $("body").css("overflow-y", "scroll");
+            window.scrollTo(0, 0);
+            $('html, body').css({
+                'overflow': 'auto',
+                'height': 'auto'
+            });
     	}
     	else{
+            window.scrollTo(0, 0);
+            $('html, body').css({
+                'overflow': 'hidden',
+                'height': '100%'
+            });
     		var p = $(".nav").position();
     		p.left += $(".menu").width() + 380;
     		p.top += $(".nav").height();
-    		$("#left-menu-dialog").width(380);
+    		$("#left-menu-dialog").width(380).css("margin-left", "80px");
     		$("#cover").css("display", "inline-block").css("left", p.left.toString()).css("top", p.top.toString()).width("100%").height($(".menu").height());
     	}
 	}
+    $("#cover").bind("click", function(){
+        $("#left-menu-dialog").width(0);
+        $("#cover").css("display", "none");
+    })
 });
