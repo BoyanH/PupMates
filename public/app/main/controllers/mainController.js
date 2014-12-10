5b8c54665065b5b0ee975f5f0da21a7e3ff3a1e3
unknown <alexanderyordanov96@gmail.com>
2014-12-10 22:46:18 +0200

    Chat Icon Left Menu Done

--- public/app/main/controllers/mainController.js
+++ public/app/main/controllers/mainController.js
@@ -2,6 +2,4 @@
 app.controller('MainController', function($scope, identity){
 	var height = $(document).height() - $(".nav").height();
 	$(".menu").css("height", height.toString());
-
-
 });
--- public/app/main/controllers/menuController.js
+++ public/app/main/controllers/menuController.js
@@ -1,8 +1,9 @@
 app.controller("MenuController", function($scope){
 	$("#left-menu-dialog").height($(document).height()-$(".nav").height());
 
-    $scope.showDialog = function(){
+    $scope.showDialog = function(e){
     	var wd = $("#left-menu-dialog").width();
+        console.log(e);
     	if(wd == 380){
     		$("#left-menu-dialog").width(0);
     		$("#cover").css("display", "none");
@@ -12,6 +13,7 @@ app.controller("MenuController", function($scope){
                 'overflow': 'auto',
                 'height': 'auto'
             });
+            $(".chat-btn").css("background-color", "#505050");
 
     	}
     	else{
@@ -20,6 +22,7 @@ app.controller("MenuController", function($scope){
                 'overflow': 'hidden',
                 'height': '100%'
             });
+            $(".chat-btn").css("background-color", "#e1e1e1");
             $(".right-wrapper").css("margin-left", "0");
     		var p = $(".nav").position();
     		p.left += $(".menu").width() + 380;
