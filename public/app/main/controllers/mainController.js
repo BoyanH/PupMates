7fe3d68f8689a921d290cbc4e44c16859d290e6a
unknown <alexanderyordanov96@gmail.com>
2014-12-10 03:15:51 +0200

    Almost done left menu dialog

--- public/app/main/controllers/mainController.js
+++ public/app/main/controllers/mainController.js
@@ -2,5 +2,6 @@
 app.controller('MainController', function($scope, identity){
 	var height = $(document).height() - $(".nav").height();
 	$(".menu").css("height", height.toString());
-	$(".wrapper").css("margin-top", $("nav").height().toString());
+
+
 });
--- public/app/main/controllers/menuController.js
+++ public/app/main/controllers/menuController.js
@@ -1,3 +1,17 @@
 app.controller("MenuController", function($scope){
-	
+	$("#left-menu-dialog").height($(document).height()-$(".nav").height() - 3);
+    $scope.showDialog = function(){
+    	var wd = $("#left-menu-dialog").width();
+    	if(wd == 380){
+    		$("#left-menu-dialog").width(0);
+    		$("#cover").css("display", "none");
+    	}
+    	else{
+    		var p = $(".nav").position();
+    		p.left += $(".menu").width() + 380;
+    		p.top += $(".nav").height();
+    		$("#left-menu-dialog").width(380);
+    		$("#cover").css("display", "inline-block").css("left", p.left.toString()).css("top", p.top.toString()).width("100%").height($(".menu").height());
+    	}
+	}
 });
