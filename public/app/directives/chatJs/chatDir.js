42f07ee88b67b05410ac3677442333ba9d705f0d
BoyanH <boqnh@abv.bg>
2015-01-23 02:29:50 +0200

    left menu closes on chat-open, exceptions handled

--- public/app/directives/chatJs/chatDir.js
+++ public/app/directives/chatJs/chatDir.js
@@ -2,6 +2,16 @@
 app.directive("chatDir", function(){
 	return{
 		restrict: "EA",
-		templateUrl: "partials/directives/chatDir"
+		templateUrl: "partials/directives/chatDir",
+		controller: function ($scope) {
+
+			$scope.hideModal = function () {
+
+				setTimeout(function() {
+			        
+			        angular.element('#cover').trigger('click');
+			    }, 0);
+			}
+		}
 	}
 })
