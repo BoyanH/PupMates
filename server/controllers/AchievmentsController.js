c478a50afe183b9070da143e333803cc6f300694
BoyanH <boqnh@abv.bg>
2015-02-25 14:20:07 +0200

    added buttons hide on profile route

--- server/controllers/AchievmentsController.js
+++ server/controllers/AchievmentsController.js
@@ -30,7 +30,7 @@ module.exports = {
             };
 
 
-        YoutubeCtrl.postVideo(req.video.data)
+        YoutubeCtrl.postVideo(achievment.video.data)
         .then(function (data) {
 
             console.log(data);
