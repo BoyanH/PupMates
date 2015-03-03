cdfe31d1ad9e356c05e22f281218fd08eb36da28
unknown <alexanderyordanov96@gmail.com>
2015-03-03 17:02:23 +0200

    Fixed bug in profile controller

--- public/app/profile/controllers/profileController.js
+++ public/app/profile/controllers/profileController.js
@@ -18,35 +18,34 @@ app.controller('ProfileRouteController', function($scope, $location, identity,
         console.log($scope.profile);
         if($scope.identity.currentUser)
             console.log($scope.identity.currentUser.dogs);
-    });
-    $scope.goToDogRoute = function(ind){
-        $location.path('/dog/' + $scope.profile.dogs[ind]._id, true);
-    }
-    $scope.changeView = function(view) {
-        $location.path('/profile/' + $scope.profile.username + '/' + view, false);
-
-        for(var prop in $scope.view) {
-
-            $scope.view[prop] = false;
+        $scope.goToDogRoute = function(ind){
+            $location.path('/dog/' + $scope.profile.dogs[ind]._id, true);
         }
+        $scope.changeView = function(view) {
+            $location.path('/profile/' + $scope.profile.username + '/' + view, false);
+
+            for(var prop in $scope.view) {
+
+                $scope.view[prop] = false;
+            }
         
-        $scope.view[view] = true;
-    }
-    $scope.befriendMate = function () {
+            $scope.view[view] = true;
+        }
+        $scope.befriendMate = function () {
 
-       	requester.addFriend($scope.profile._id, $scope.profile.username)
-        .then(function (data) {
+            requester.addFriend($scope.profile._id, $scope.profile.username)
+            .then(function (data) {
 
-            if(data == 'requested') {
-                notifier.success("A friend request was sent!");
-            }
-            else if(data == 'added') {
-                notifier.success('Friendship accepted!');
-            }
-            else if(data == 'already exists') {
+                if(data == 'requested') {
+                    notifier.success("A friend request was sent!");
+                }
+                else if(data == 'added') {
+                    notifier.success('Friendship accepted!');
+                }
+                else if(data == 'already exists') {
                 notifier.error('This user is already in your friends list!');
-            }
-        });
-    }
-
+                }
+            });
+        }
+    });
 });
