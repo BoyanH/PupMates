e1ddb4d6c39a327c8bd6fd9017ebf0bb2d3e0e6e
unknown <alexanderyordanov96@gmail.com>
2015-04-15 23:26:42 +0300

    Menu open and close done

--- phonegap/www/js/app.js
+++ phonegap/www/js/app.js
@@ -1,3 +1,13 @@
+$(document).on("pagecreate", function () {
+    $("[data-role=panel]").one("panelbeforeopen", function () {
+        var height = $.mobile.pageContainer.pagecontainer("getActivePage").outerHeight();
+        $(".ui-panel-wrapper").css("height", height + 1);
+    });
+});
+$(function () {
+    $("[data-role=header],[data-role=footer]").toolbar().enhanceWithin();
+    $("[data-role=panel]").panel().enhanceWithin();
+});
 function loginForm(){
 	var uname = $('[name="uname"]').val();
 	var pass = $('[name="password"]').val();
@@ -5,12 +15,12 @@ function loginForm(){
 	window.localStorage.setItem("username", uname);
 	window.localStorage.setItem("password", pass);
 
-	$.mobile.changePage("#home", {reverse: false, transition: "slide"});
+	$.mobile.changePage("#home-page", {reverse: false, transition: "slide"});
 
 	$("#output").html("Username: " + window.localStorage.getItem("username") 
 		+ "<br>" + "Password: " + window.localStorage.getItem("password"));
 	return false;
 }
 function goHome(){
-	$.mobile.changePage("#home", {reverse: false, transition: "slide"});
+	$.mobile.changePage("#home-page", {reverse: false, transition: "slide"});
 }
